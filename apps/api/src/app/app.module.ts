78714c777af19a9f3f1ac8b81c6b348f4c71cd6c
Afif Alfiano <afifalfiano2@gmail.com>
2026-06-21 14:39:19 +0700

    feat(p2): add ItemsModule skeleton

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
+import { ItemsModule } from './items/items.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ItemsModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
